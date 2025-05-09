11ea49993bac2b1d30c27e82a1a0c3af76e841d9
magistak <magistak@gmail.com>
2025-05-09 12:53:04 +0200

    Remove dataguy/ after migration to src/, update tests and demo

--- dataguy/__init__.py
+++
@@ -1,6 +0,0 @@
-from .core import DataGuy
-from .context_manager import ContextManager
-from .utils import LLMResponseCache
-
-__version__ = "0.1.0"
-__all__ = ["DataGuy", "ContextManager", "LLMResponseCache"]
